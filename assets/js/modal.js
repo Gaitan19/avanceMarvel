595ba1b708a446feb936ea3c7048db142da70612
kenley.gaitan <kenley.gaitan@smbssolutions.com>
2023-11-28 11:10:08 -0600

    it was created the modal

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,14 +1,24 @@
-document.querySelectorAll(".Button-modal").forEach((el) => {
-  el.addEventListener("click", function (ev) {
-    ev.stopPropagation();
-    this.parentNode.classList.add("Modal-active");
-    console.log("di click");
-  });
-});
+document.addEventListener("DOMContentLoaded", function () {
+  const modal = document.getElementById("modal");
+  const modalImg = document.getElementById("modalImage");
+  const buttons = document.querySelectorAll(".Button-modal");
+  const closeModal = document.getElementById("closeModal");
 
-document.querySelectorAll(".Modal").forEach((el) => {
-  el.addEventListener("click", function (ev) {
-    this.classList.remove("Modal-active");
-    console.log("Click");
+  buttons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      const imageSrc = this.parentNode.querySelector(".Gallery-image").src;
+      modal.style.display = "flex";
+      modalImg.src = imageSrc;
+    });
+  });
+
+  closeModal.addEventListener("click", function () {
+    modal.style.display = "none";
+  });
+
+  window.addEventListener("click", function (event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
   });
 });
